e76ff238dba90571d6d44600f977da35127e7385
VANDAN GHORI <vandanghori565@gmail.com>
2025-10-05 20:22:16 +0530

    Update review.js

--- routes/review.js
+++ routes/review.js
@@ -4,10 +4,8 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const {validateReview, isLoggedIn,isReviewAuthor} = require("../middleware.js");
 const reviewController = require("../controllers/reviews.js");
 
-//Post Reviews Route
 router.post("/", isLoggedIn,validateReview,wrapAsync(reviewController.postReview));
 
-//Delete Review Route
 router.delete("/:reviewId", isLoggedIn,isReviewAuthor, wrapAsync(reviewController.destroyReview));
 
 module.exports = router;
